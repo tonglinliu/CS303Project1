c1a243f0f3b004c4280627d5a1ce6e67ed574924
leopoldkeiser <33555320+leopoldkeiser@users.noreply.github.com>
2018-06-25 20:01:28 -0500

    added Library class and function declarations

--- CS303Project1/CS303Project1/Library.cpp
+++ CS303Project1/CS303Project1/Library.cpp
@@ -10,3 +10,19 @@ Library::Library()
 Library::~Library()
 {
 }
+
+void Library::add_book(std::string s)
+{
+}
+
+void Library::add_employee(std::string s)
+{
+}
+
+void Library::circulate_book(std::string s, Date d)
+{
+}
+
+void Library::pass_on(std::string s, Date d)
+{
+}
--- CS303Project1/CS303Project1/Library.h
+++ CS303Project1/CS303Project1/Library.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <list>
+#include "Date.h"
 
 
 class Library
@@ -9,8 +10,14 @@ public:
 	Library();
 	~Library();
 
+	void add_book(std::string s);
+	void add_employee(std::string s);
+
+	void circulate_book(std::string s, Date d);
+	void pass_on(std::string s, Date d);
+
+
 private:
-
-
+	
 };
 
