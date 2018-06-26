b0ad26e49773f0edfbd548c2a28f7c563c5caabb
leopoldkeiser <33555320+leopoldkeiser@users.noreply.github.com>
2018-06-25 19:28:37 -0500

    added getters and setters

--- CS303Project1/CS303Project1/Employee.h
+++ CS303Project1/CS303Project1/Employee.h
@@ -8,6 +8,13 @@ public:
 	// Constructor
 	Employee(std::string theName);
 
+	std::string getName() { return name; }
+	int getWaitingTime() { return waiting_time; }
+	int getRetainingTime() { return retaining_time; }
+
+	void setWaitingTime(int w) { waiting_time = w; }
+	void setRetainingTime(int r) { retaining_time = r; }
+
 private:
 	std::string name;
 	int waiting_time;
