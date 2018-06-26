d69e1bf9f6e37092fb3538d689ac9b50ef361bba
leopoldkeiser <33555320+leopoldkeiser@users.noreply.github.com>
2018-06-25 19:54:00 -0500

    added additional header file required by Date class

---
+++ CS303Project1/CS303Project1/Library.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Library.h"
+
+
+Library::Library()
+{
+}
+
+
+Library::~Library()
+{
+}
---
+++ CS303Project1/CS303Project1/Library.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <list>
+
+
+class Library
+{
+public:
+	Library();
+	~Library();
+
+private:
+
+
+};
+
